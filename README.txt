dfb266dc78fd588e2324f28bbf59f3de081d688e
Mohit <mohitindubai@gmail.com>
2011-06-12 01:08:39 +0530

    G-Caprice

--- README.txt
+++ README.txt
@@ -1,7 +1,25 @@
 G-Caprice LAN Chat
 Version: 1.1
 Developed by: Mohit Gvalani
+Email: mohitindubai@gmail.com
+Copyright © 2009, Mohit Gvalani
 
+License Agreement:
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+Description:
 This is a Java based chat application. It has been tested on WindowsXP with JRE6 installed. 
 This application has been developed for chatting over Local Area Networks(LAN) on Windows PCs.
 
--- src/G_Caprice_LAN_1_1.java
+++ src/G_Caprice_LAN_1_1.java
@@ -1,5 +1,18 @@
+/*
+*Copyright 2010, Mohit Gvalani
+
+*This file is part of G-Caprice.
+*G-Caprice is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+*G-Caprice is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+*You should have received a copy of the GNU General Public License along with G-Caprice.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+
+
 import java.awt.*;				// Package(that contains classes) for Graphical components
-import java.awt.event.*;		// Package(that contains classes) for Events like mouse click, key press, etc.
+import java.awt.event.*;			// Package(that contains classes) for Events like mouse click, key press, etc.
 import java.net.*;				// Package(that contains classes) for networking
 import java.io.*;				// Package(that contains classes) for input & output
 
